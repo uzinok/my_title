38d230dc5533bc7714194c8bb8895c44f3530867
uzinok <uzinok@yandex.ru>
2020-11-07 19:25:33 +0400

    test 07.11.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 window.onload = () => {
-    console.log(`"hello" - 'world'`)
+    let titles = document.querySelectorAll('[title]');
+
+    titles.forEach(element => {
+        element.setAttribute('data-title', element.getAttribute('title'));
+        element.removeAttribute('title');
+        let check = false;
+
+        element.addEventListener('mouseover', (e) => {
+            if (!check) {
+                check = true;
+                let titleText = document.createElement('span');
+                titleText.classList.add('title-text');
+
+                console.dir(e.target)
+
+                titleText.style.left = e.clientX + 20 + "px";
+                titleText.style.top = e.target.offsetTop - 25 + "px";
+                titleText.innerHTML = e.target.getAttribute('data-title');
+
+                document.body.append(titleText);
+
+                e.target.addEventListener('mouseout', () => {
+                    titleText.remove();
+                    check = false;
+                })
+            }
+
+        })
+    });
 };
